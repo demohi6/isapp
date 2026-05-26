a3447e2cb8d5440277352a7f0ba08e0e49e6108e
demohi6 <88313240+demohi6@users.noreply.github.com>
2026-05-26 15:40:08 +0400

    chore: bump sw.js to trigger Pages redeploy

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-// Unregister all caches and self
+// v9 - cache bust
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
-    .then(() => self.clients.matchAll().then(clients => 
+    .then(() => self.clients.matchAll().then(clients =>
       clients.forEach(c => c.navigate(c.url))
     ))
   );
